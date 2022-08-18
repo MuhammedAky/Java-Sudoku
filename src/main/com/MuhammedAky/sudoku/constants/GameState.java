227e5d9a4bcddd49955f5ae2c93253369708d1d4
MuhammedAky <muhammedaky24@gmail.com>
2022-08-18 12:14:51 +0300

    Finished

---
+++ src/main/com/MuhammedAky/sudoku/constants/GameState.java
@@ -0,0 +1,7 @@
+package main.com.MuhammedAky.sudoku.constants;
+
+public enum GameState {
+    COMPLETE,
+    ACTIVE,
+    NEW
+}
---
+++ src/main/com/MuhammedAky/sudoku/constants/Rows.java
@@ -0,0 +1,7 @@
+package main.com.MuhammedAky.sudoku.constants;
+
+public enum Rows {
+    TOP,
+    MIDDLE,
+    BOTTOM
+}
